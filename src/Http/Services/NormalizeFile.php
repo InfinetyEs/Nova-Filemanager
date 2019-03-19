a5187962f7df840c6f54538c46d703628e9d1613
Eric Lagarda <info@ericlagarda.com>
2019-03-19 09:34:38 +0000

    Apply fixes from StyleCI

--- src/Http/Services/NormalizeFile.php
+++ src/Http/Services/NormalizeFile.php
@@ -2,12 +2,12 @@
 
 namespace Infinety\Filemanager\Http\Services;
 
-use Carbon\Carbon;
-use Illuminate\Filesystem\FilesystemAdapter;
-use Illuminate\Support\Collection;
 use RarArchive;
-use SplFileInfo;
 use ZipArchive;
+use SplFileInfo;
+use Carbon\Carbon;
+use Illuminate\Support\Collection;
+use Illuminate\Filesystem\FilesystemAdapter;
 
 class NormalizeFile
 {
@@ -164,7 +164,7 @@ class NormalizeFile
         if (str_contains($mime, 'image')) {
             list($width, $height) = getimagesize($this->storage->path($this->storagePath));
 
-            if (!empty($width) && !empty($height)) {
+            if (! empty($width) && ! empty($height)) {
                 return $width.'x'.$height;
             }
         }
