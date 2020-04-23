ead95eff1104e9e2ff99561712d82c8d70118375
piterssson <piotr.swidurski@gmail.com>
2020-04-23 12:24:22 +0200

    readonly

--- resources/js/field/custom/FileSelect.vue
+++ resources/js/field/custom/FileSelect.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-wrap items-stretch w-full mb-2 relative">
-        <input hidden type="text" class="flex-shrink flex-grow flex-auto leading-normal w-px flex-1 form-control form-input form-input-bordered-l relative"
+        <input readonly type="text" class="flex-shrink flex-grow flex-auto leading-normal w-px flex-1 form-control form-input form-input-bordered-l relative"
                :placeholder="__('Select a file')" v-model="value">
         <div class="flex -mr-px">
             <span class="filemanager-open flex items-center leading-normal rounded-lg rounded-l-none border border-l-0 border-grey-light bg-40 px-3 whitespace-no-wrap text-grey-dark text-sm cursor-pointer" @click="openModalFilemanager">{{ __('Open FileManager') }}</span>
